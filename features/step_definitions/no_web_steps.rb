4a7bcc3ac9ac228224cbd0cf6183e3d4c27426ce
Mario Visic <mario@mariovisic.com>
2011-10-25 23:22:15 +0800

    Small fix.

--- features/step_definitions/no_web_steps.rb
+++ features/step_definitions/no_web_steps.rb
@@ -1,11 +1,11 @@
 Then /^I should be able to login$/ do
   visit new_user_session_path
-  Then %{I fill in my details}
+  fill_in_my_details
   click_button 'Sign in'
   page.should have_content 'Signed in'
 end
 
-Then /^I fill in my details$/ do
+def fill_in_my_details
   fill_in 'Email', :with => @current_email
   fill_in 'Password', :with => @current_password
 end
