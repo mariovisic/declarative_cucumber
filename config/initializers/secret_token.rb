1a8a82c4c34a85335f279a3150e8df36b72eafd1
Mario Visic <mario@mariovisic.com>
2011-10-25 22:04:58 +0800

    Initial setup, cucumber install.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CucumberExamples::Application.config.secret_token = '63698c9faa76ce1cade50a7dd7fcfae48b66f784db2bb191022170978d694b26025f03f64b12be07b9a662a4820b2bd2d7c86b335e6aad9b1b21f3b1518bb778'
